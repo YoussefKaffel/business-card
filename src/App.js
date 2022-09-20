2d2cd9ffd8d426fa08828dc8397635edee560592
YoussefKaffel <youssef.kaffel99@gmail.com>
2022-09-20 19:08:55 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.scss';
+import Image from './assets/DSC_3213.jpg';
+import Btn from './btn'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="bgtint"></div>
+      <section className="bussinesscard">
+        <div className="flip">
+          <div className="front">
+            <div className="top">
+              <div className="logo"><img src={Image}/></div>
+            </div>
+            <div className="nametroduction">
+              <div className="name">Youssef Kaffel</div>
+              <div className="introduction">Junior Developer</div>
+
+
+
+            </div>
+            <div className="contact">
+              <div className="website">
+                <span className="ion-earth"></span>
+                <a href="https://youssefkaffel.github.io/portfolio/">https://youssefkaffel.github.io/portfolio</a>
+              </div>
+              <div className="twitter">
+                <span className="ion-social-linkedin"></span>
+                <a href="https://www.linkedin.com/in/youssefkaffel/">youssefkaffel</a>
+              </div>
+              <div className="phone ">
+                <span className="ion-ios7-telephone"></span>
+                <a href="tel:23911695">(+216) 23 911 695</a>
+              </div>
+              <div className="phone ">
+                <span className="ion-social-github"></span>
+                <a href="https://github.com/YoussefKaffel">YoussefKaffel</a>
+              </div>
+              <div className="email ">
+                <span className="ion-paper-airplane"></span>
+                <a href="mailto:youssef.kaffel99@gmail.com">youssef.kaffel99@gmail.com</a>
+              </div>
+            </div>
+          </div>
+          <div className="back"></div>
+        </div>
+      </section>
+      <section className="tooltip-web">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hover over the card to see more.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </section>
+      <section className="tooltip-mobile">
+        <p>
+          Click on the card to see more & Click outside to close.
+        </p>
+      </section>
     </div>
   );
 }
